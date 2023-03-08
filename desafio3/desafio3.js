f2eda40caa696b65333dc8cedc0f3bc656724826
Lucas <116681825+lucassilva3g@users.noreply.github.com>
2023-03-08 12:07:44 -0300

    Update desafio3.js

--- desafio3/desafio3.js
+++ desafio3/desafio3.js
@@ -1,40 +1,36 @@
-const fs = require('fs');
+// a) A lógica é adicionar 2 ao número anterior, portanto o próximo elemento é 9.
 
-const dados = JSON.parse(fs.readFileSync('dados.json'));
+let a = [1, 3, 5, 7];
+let nextA = a[a.length - 1] + 2;
+console.log(nextA); // Saída: 9
 
-let menorFaturamento = Infinity;
-let maiorFaturamento = -Infinity;
+// b) A lógica é multiplicar o número anterior por 2, portanto o próximo elemento é 128.
 
-let somaFaturamento = 0;
-let diasComFaturamento = 0;
+let b = [2, 4, 8, 16, 32, 64];
+let nextB = b[b.length - 1] * 2;
+console.log(nextB); // Saída: 128
 
-dados.forEach((dia) => {
-  const valor = dia.valor;
+// c) A lógica é adicionar uma sequência de números ímpares a partir de 1, portanto o próximo elemento é 49.
 
-  if (valor < menorFaturamento) {
-    menorFaturamento = valor;
-  }
+let c = [0, 1, 4, 9, 16, 25, 36];
+let nextC = c[c.length - 1] + (c.length * 2) + 1;
+console.log(nextC); // Saída: 49
 
-  if (valor > maiorFaturamento) {
-    maiorFaturamento = valor;
-  }
+// d) A lógica é adicionar uma sequência de números pares a partir de 4, portanto o próximo elemento é 100.
 
-  if (valor > 0) {
-    somaFaturamento += valor;
-    diasComFaturamento += 1;
-  }
-});
+let d = [4, 16, 36, 64];
+let nextD = d[d.length - 1] + (d.length * 4);
+console.log(nextD); // Saída: 100
 
-const mediaMensal = somaFaturamento / diasComFaturamento;
+//e) A lógica é adicionar os dois números anteriores, portanto o próximo elemento é 13.
 
-let diasAcimaMedia = 0;
+let e = [1, 1, 2, 3, 5, 8];
+let nextE = e[e.length - 1] + e[e.length - 2];
+console.log(nextE); // Saída: 13
 
-dados.forEach((dia) => {
-  if (dia.valor > mediaMensal) {
-    diasAcimaMedia += 1;
-  }
-});
+// f) A lógica não é muito clara, mas aparentemente os números estão em ordem crescente com exceção do número 17. Dessa forma, podemos supor que o próximo número é 20.
 
-console.log(`Menor valor de faturamento: R$ ${menorFaturamento.toFixed(2)}`);
-console.log(`Maior valor de faturamento: R$ ${maiorFaturamento.toFixed(2)}`);
-console.log(`Número de dias com faturamento acima da média: ${diasAcimaMedia}`);
+let f = [2, 10, 12, 16, 17, 18, 19];
+let nextF = 20;
+console.log(nextF); // Saída: 20
+
