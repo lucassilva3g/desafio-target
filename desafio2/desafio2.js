6cbdfd463f6b5ddf89ff0c621c06b77422466f4f
Lucas <116681825+lucassilva3g@users.noreply.github.com>
2023-03-08 12:04:00 -0300

    fibonnacci

--- desafio2/desafio2.js
+++ desafio2/desafio2.js
@@ -1,8 +1,31 @@
-let INDICE = 13, SOMA = 0, K = 0;
+let num = parseInt(prompt("Digite um número:"));
 
-while (K < INDICE) {
-  K = K + 1;
-  SOMA = SOMA + K;
+let fib1 = 0, fib2 = 1, nextFib;
+
+while (fib1 <= 100) {
+  console.log(fib1);
+  nextFib = fib1 + fib2;
+  fib1 = fib2;
+  fib2 = nextFib;
 }
 
-console.log(SOMA); // Saída: 91
+if (fibonacci(num)) {
+  console.log(num + " pertence à sequência de Fibonacci.");
+} else {
+  console.log(num + " não pertence à sequência de Fibonacci.");
+}
+
+function fibonacci(n) {
+  let fib1 = 0, fib2 = 1, nextFib;
+
+  while (fib1 <= n) {
+    if (fib1 === n) {
+      return true;
+    }
+    nextFib = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = nextFib;
+  }
+
+  return false;
+}
