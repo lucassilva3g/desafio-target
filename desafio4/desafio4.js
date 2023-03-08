93c6dfcfb63266fa3791dc608ac7f40f25109f10
Lucas <116681825+lucassilva3g@users.noreply.github.com>
2023-03-08 12:17:17 -0300

    Update desafio4.js

--- desafio4/desafio4.js
+++ desafio4/desafio4.js
@@ -1,19 +1,31 @@
-const faturamentoPorEstado = {
-  'SP': 67836.43,
-  'RJ': 36678.66,
-  'MG': 29229.88,
-  'ES': 27165.48,
-  'Outros': 19849.53
-};
+// Distância entre Ribeirão Preto e Franca (em km)
+const distanciaTotal = 100;
 
-const valorTotal = Object.values(faturamentoPorEstado).reduce((total, faturamento) => total + faturamento, 0);
+// Velocidade constante do carro (em km/h)
+const velocidadeCarro = 110;
 
-const percentuaisPorEstado = {};
+// Velocidade constante do caminhão (em km/h)
+const velocidadeCaminhao = 80;
 
-for (const estado in faturamentoPorEstado) {
-  const faturamento = faturamentoPorEstado[estado];
-  const percentual = (faturamento / valorTotal) * 100;
-  percentuaisPorEstado[estado] = percentual.toFixed(2) + '%';
+// Tempo que o caminhão leva a mais para passar em cada pedágio (em minutos)
+const tempoPedagio = 5;
+
+// Converter o tempo do pedágio para horas
+const tempoPedagioEmHoras = tempoPedagio / 60;
+
+// Calcular o tempo de viagem do carro até o encontro com o caminhão
+const tempoCarro = distanciaTotal / velocidadeCarro;
+
+// Calcular o tempo de viagem do caminhão até o encontro com o carro
+const tempoCaminhao = (distanciaTotal / velocidadeCaminhao) + (2 * tempoPedagioEmHoras);
+
+// Multiplicar a velocidade pela duração da viagem para obter a distância percorrida por cada veículo
+const distanciaPercorridaCarro = velocidadeCarro * tempoCarro;
+const distanciaPercorridaCaminhao = velocidadeCaminhao * tempoCaminhao;
+
+// Verificar qual veículo está mais próximo de Ribeirão Preto
+if (distanciaPercorridaCarro === distanciaTotal) {
+  console.log('O carro está mais próximo de Ribeirão Preto.');
+} else {
+  console.log('O caminhão está mais próximo de Franca.');
 }
-
-console.log(percentuaisPorEstado);
